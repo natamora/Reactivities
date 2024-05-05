db570157091812c4ae3bf685c0daee2a0b9d38b1
Natamora <natamora96@gmail.com>
2024-05-05 04:37:39 +0200

    End of Section 10: Error handling + Fluent Validation + Handling API error responses + Handling exceptions + error handling in the client app

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain
 {
     public class Activity
     {
         public Guid Id { get; set; }
+
+        // [Required] - allow automatic HTTP 400 responses if we have [ApiController] attribute
         public string Title { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
