f9fb8795964d563659d29fb1c84e4b034a5c0886
Natamora <natamora96@gmail.com>
2024-06-20 00:12:23 +0200

    End of Section 17: Client image upload + profile page + displaying user photos + photo upload widget + react dropzone and react cropper + setting main photo + deleting photos

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -7,21 +7,23 @@ import TestErrors from "../../features/errors/TestError";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
 import LoginForm from "../../features/users/LoginForm";
+import ProfilePage from "../../features/profiles/ProfilePage";
 
 export const routes: RouteObject[] = [
     {
         path: '/',
         element: <App />,
         children: [
-            {path: 'activities', element: <ActivityDashboard />},
-            {path: 'activities/:id', element: <ActivityDetails />},
-            {path: 'createActivity', element: <ActivityForm key='create' />},
-            {path: 'manage/:id', element: <ActivityForm key='manage'/>},
-            {path: 'login', element: <LoginForm />},
-            {path: 'errors', element: <TestErrors />},
-            {path: 'not-found', element: <NotFound />},
-            {path: 'server-error', element: <ServerError />},
-            {path: '*', element: <Navigate replace to='/not-found' />},
+            { path: 'activities', element: <ActivityDashboard /> },
+            { path: 'activities/:id', element: <ActivityDetails /> },
+            { path: 'createActivity', element: <ActivityForm key='create' /> },
+            { path: 'manage/:id', element: <ActivityForm key='manage' /> },
+            { path: 'profiles/:username', element: <ProfilePage /> },
+            { path: 'login', element: <LoginForm /> },
+            { path: 'errors', element: <TestErrors /> },
+            { path: 'not-found', element: <NotFound /> },
+            { path: 'server-error', element: <ServerError /> },
+            { path: '*', element: <Navigate replace to='/not-found' /> },
 
         ]
     },
--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -12,28 +12,29 @@ export default function ActivityListItem({ activity }: Props) {
     return (
         <Segment.Group>
             <Segment>
-                {activity.isCancelled && 
-                    <Label attached='top' color='red' content='Cancelled' style={{textAlign: 'center'}}/>
+                {activity.isCancelled &&
+                    <Label attached='top' color='red' content='Cancelled' style={{ textAlign: 'center' }} />
                 }
                 <Item.Group>
                     <Item>
-                        <Item.Image style={{marginBottom: 3}} size='tiny' circular src='/assets/user.png' />
+                        <Item.Image style={{ marginBottom: 3 }} size='tiny' circular
+                            src={activity.host?.image || '/assets/user.png'} />
                         <Item.Content>
                             <Item.Header as={Link} to={`/activities/${activity.id}`}>
                                 {activity.title}
                             </Item.Header>
-                            <Item.Description> Hosted by {activity.host?.displayName}</Item.Description>
+                            <Item.Description> Hosted by <Link to={`/profiles/${activity.hostUsername}`}>{activity.host?.displayName}</Link></Item.Description>
                             {activity.isHost && (
                                 <Item.Description>
                                     <Label basic color='orange'>
-                                    You are hosting this activity
+                                        You are hosting this activity
                                     </Label>
                                 </Item.Description>
                             )}
                             {activity.isGoing && !activity.isHost && (
                                 <Item.Description>
                                     <Label basic color='green'>
-                                    You are going to this activity
+                                        You are going to this activity
                                     </Label>
                                 </Item.Description>
                             )}
@@ -43,16 +44,16 @@ export default function ActivityListItem({ activity }: Props) {
             </Segment>
             <Segment>
                 <span>
-                    <Icon name='clock'/> {format(activity.date!, 'dd MMM yyyy h:mm aa')}
-                    <Icon name='marker'/> {activity.venue}
+                    <Icon name='clock' /> {format(activity.date!, 'dd MMM yyyy h:mm aa')}
+                    <Icon name='marker' /> {activity.venue}
                 </span>
             </Segment>
             <Segment secondary>
-                <ActivityListItemAttendee attendees={activity.attendees!}/>
+                <ActivityListItemAttendee attendees={activity.attendees!} />
             </Segment>
             <Segment clearing>
                 <span>{activity.description}</span>
-                <Button 
+                <Button
                     as={Link}
                     to={`/activities/${activity.id}`}
                     color='teal'
