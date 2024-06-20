f435066fb85d0fe8ef22e008602f4c7b7a517ecd
Natamora <natamora96@gmail.com>
2024-06-20 22:44:03 +0200

    End of Section 19: SignalR + live chat with comments in activity page + SignalR Hub to send comments + authenticating to SignalR + SignalR client side + validation

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -177,4 +177,7 @@ export default class ActivityStore {
             runInAction(() => this.loading = false);
         }
     }
+    clearSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    }
 }
