56c1801be1efd043eaef2f978719ed9f2b000a68
Natamora <natamora96@gmail.com>
2024-06-27 13:54:56 +0200

    Email verification by SendGrid

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -3,6 +3,7 @@ import { User, UserFormValues } from "../models/user";
 import agent from "../api/agent";
 import { store } from "./store";
 import { router } from "../router/Routes";
+import { isAxiosError } from "axios";
 
 export default class UserStore {
     user: User | null = null;
@@ -26,12 +27,15 @@ export default class UserStore {
     }
 
     register = async (creds: UserFormValues) => {
-        const user = await agent.Account.register(creds);
-        store.commonStore.setToken(user.token);
-        this.startRefreshTokenTimer(user);
-        runInAction(() => this.user = user);
-        router.navigate('/activities');
-        store.modalStore.closeModal();
+        try {
+            await agent.Account.register(creds);
+            router.navigate(`/account/registerSuccess?email=${creds.email}`);
+            store.modalStore.closeModal();
+        } catch (error) {
+            if (isAxiosError(error) && error?.response?.status === 400) throw error;
+            store.modalStore.closeModal();
+            console.log(500);
+        }
     }
 
     logout = () => {
